7c9372fa6b3097fe6849b1f50fca53cae13c757d
Insup Jung <31408300+jis1218@users.noreply.github.com>
2017-09-04 14:49:53 +0900

    first commit

---
+++ src/HelloMain.java
@@ -0,0 +1,17 @@
+
+public class HelloMain {
+	
+		//접근제한자, Return 타입, 함수명(String 파라미터)
+		//public
+		//private 자기 클래스끼리만
+		//protected
+		//default
+	
+	
+	public static void main(String args[]){
+		System.out.println("Hello Java!!!");
+		
+	}
+	
+	
+}
